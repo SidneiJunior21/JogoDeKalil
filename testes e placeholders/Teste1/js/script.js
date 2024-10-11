cf49f7bdb3b70c6d7478549ec680026af9898082
SidneiJunior21 <sidnei.junior@dcomp.ufs.br>
2024-10-11 10:51:21 -0300

    1

--- testes e placeholders/Teste1/js/script.js
+++ testes e placeholders/Teste1/js/script.js
@@ -1,4 +1,17 @@
-// funçao q vai guiar a introdução:
-const intro = () => {
-    $("#botao1").addEventListener("click",)
+// funções q vai guiar a introdução:
+
+let bot = document.getElementById("bot1")
+bot.addEventListener("click",skip2);
+
+function skip1 () {
+
+}
+
+
+function skip2 () {
+//    alt()
+//    $("#texto").innerText="testando testando";
+    const textos = document.getElementById("texto");
+    textos.textContent="newtext";
+//    document.getElementById("myspan");
 }
