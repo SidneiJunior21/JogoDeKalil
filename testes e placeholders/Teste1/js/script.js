5bfd8276c27221abbcdf045d4c529e2ea90afae9
SidneiJunior21 <sidnei.junior@dcomp.ufs.br>
2024-10-11 14:30:59 -0300

    primeiro diálogo adicionado

--- testes e placeholders/Teste1/js/script.js
+++ testes e placeholders/Teste1/js/script.js
@@ -1,14 +1,31 @@
 // funções q vai guiar a introdução:    
-
-function skip1() {
+function introT1() {
     const bot1 = document.getElementById("bot1");
-    document.getElementById("texto").innerHTML = 'cuzinho';
-    bot1.addEventListener("click", skip2)    
+    const bot2 = document.getElementById("bot2");
+    document.getElementById("texto").innerHTML = 'E não se esqueça do juramento que você fez, "Vivo para servir, servirei até a morte. e se o imperador pedir, mesmo morto vou servir". Imagino que você saiba das responsabilidades que um Sumo Oficial Fronteiriço tem, mas mesmo assim irei relembra-lo, você decide tudo que entra e sai desse planeta, permitindo a passagem de quem deve e usando das anti-aéreas em quem tiver a entrada negada.';
+    bot1.addEventListener("click", introT2);
+    bot2.addEventListener("click", introT2)
+
 }
 
-function skip2() {
-    document.getElementById("texto").innerHTML = 'newtext';
-
-//    document.getElementById("myspan");
+function introT2() {
+    const bot1 = document.getElementById("bot1");
+    const bot2 = document.getElementById("bot2");
+    document.getElementById("texto").innerHTML = 'Lembre-se, ninguém que tenha a entrada negada pode simplismente fugir, ter a entrada negada quer dizer que o Deus-Imperador nunca quis sua presença aqui e ter vindo aqui sabendo disso é heresia, e você sabe bem o que acontece com hereges, Oficial, de certo modo tem seu emprego por conta da heresia dos outros.';
+    bot1.addEventListener("click",tutorial1);
+    bot2.addEventListener("click",tutorial1)
 }
+ function tutorial1() {
+    const bot1 = document.getElementById("bot1");
+    const bot2 = document.getElementById("bot2");
+    document.getElementById("texto").innerHTML = 'Imagino que já tenha se familiarizado com a mesa de controle, o botão de cima faz com que a entrada seja permitida, o de baixo ordena as baterias anti-aéreas as disparar, tenha em mente que não temos tempo para falhas, estamos em situação crítica e qualquer erro não será tolerado, não podemos dar chance aos hereges para que nos derrubem. O que eu estou dizendo é que não há confirmação para os botões, um clique é o necessário para ter sua mensagem passada, somente aperte o botão quando tiver certo do que fazer.';
+    bot1.addEventListener("click",tutorial2);
+    bot2.addEventListener("click",tutorial2)
 
+
+}
+function tutorial2() {
+    const bot1 = document.getElementById("bot1");
+    const bot2 = document.getElementById("bot2");
+    document.getElementById("texto").innerHTML = 'Nos próximos momentos uma frota trazendo mais soldados da guarda da casa Von Velancius para garantir a ordem, permita as entradas deles, e permita assim que eles aparecerem, esses soldados são de extrema importância, em hipótese alguma dispare as baterias na frota, será executado se fizer isso. Estarei deixando você sozinho agora, sirva bem e viva bem, adeus Oficial.'
+}
