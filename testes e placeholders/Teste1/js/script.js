26d94a67c5e3c278b679bd3579b6f5f205915ca5
SidneiJunior21 <sidnei.junior@dcomp.ufs.br>
2024-10-11 13:24:09 -0300

    1

--- testes e placeholders/Teste1/js/script.js
+++ testes e placeholders/Teste1/js/script.js
@@ -1,7 +1,4 @@
 // funções q vai guiar a introdução:    
-function teste() {
-    document.getElementById("teste1").innerHTML = 'cuz'
-}
 
 function skip1() {
     const bot1 = document.getElementById("bot1");
@@ -10,10 +7,8 @@ function skip1() {
 }
 
 function skip2() {
-/*  const textos = document.getElementById("texto");
-    textos.addEventListener("click", () => textos.textContent="newtext");
-    console.log("bananinha"); */
     document.getElementById("texto").innerHTML = 'newtext';
 
 //    document.getElementById("myspan");
 }
+
