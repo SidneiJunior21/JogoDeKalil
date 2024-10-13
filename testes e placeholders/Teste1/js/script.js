bd136225809185e2ee36b2cc114e0cfcdd1c5b7d
SidneiJunior21 <sidnei.junior@dcomp.ufs.br>
2024-10-13 17:14:18 -0300

    todas as classes de nave adicionadas ao manual

--- testes e placeholders/Teste1/js/script.js
+++ testes e placeholders/Teste1/js/script.js
@@ -8,14 +8,90 @@ function manualP1() {
     setaR.onclick = manualP2
 }
 
+/*
+lista de características q uma nave pode ter: 1-classe, 2-canhões, 3-tamanho, 4-população, 5-armamentos extras, 6-pode transportar mercadorias, 7-é necessário ter licensa mercante para transportar mercadorias, 8-pode ser da Frota Imperial, 9-é obrigatoriamente da Frota Imperial, 10-pode ter armamento anti-planetário
+document.getElementById("consequenciasT").innerHTML = "Nave X <br> Canhões - Entre x até x <br> Tamanho - Entre x por x até x por x <br> População - Entre x até x mil pessoas <br> Armamentos extras - x <br> Pode transportar mercadorias? - Sim/Não <br> É necessário ter licensa mercante para transportar mercadorias? - Sim/Não <br> Pode ser da Frota Imperial? - Sim/Não <br> É obrigatoriamente da Frota Imperial? - Sim/Não <br> Pode ter armamento anti-planetário? - Somente com permissão imperial/Em nenhuma hipótese"
+*/
+
 function manualP2() {
     const setaL = document.getElementById("setaL");
     const setaR = document.getElementById("setaR");
 
-    document.getElementById("consequenciasT").innerHTML = "Nave Classe Galeão<br> teste"
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe Imperatoris</b> <br> <b>Canhões</b> - Entre 100 até 200 <br> <b>Tamanho</b> - Entre 8km por 2km até 10km por 5km <br> <b>População</b> - Entre 100 até 250 mil pessoas <br> <b>Armamentos extras</b> - Pode carregar torpedos, aríete, armas anti-projétil e canhões magnéticos <br> <b>Pode transportar mercadorias?</b> - Não <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Não é permitido transporte de mercadorias <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Sim <br> <b>Pode ter armamento anti-planetário?</b> - Deve ter"
 
     setaL.onclick = manualP1
-    setaR.onclick = manualP2
+    setaR.onclick = manualP3
+}
+function manualP3() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe Galeão</b> <br> <b>Canhões</b> - Entre 80 até 100 <br> <b>Tamanho</b> - Entre 4km por 2km até 6km por 3km <br> <b>População</b> - Entre 60 até 90 mil pessoas <br> <b>Armamentos extras</b> - Pode carregar torpedos, armas anti-projétil e um aríete <br> <b>Pode transportar mercadorias?</b> - Sim <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Sim <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Não <br> <b>Pode ter armamento anti-planetário?</b> - Somente com permissão imperial"
+
+    setaL.onclick = manualP2
+    setaR.onclick = manualP4
+}
+function manualP4() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe Cruzador Pesado</b> <br> <b>Canhões</b> - Entre 90 até 100 <br> <b>Tamanho</b> - Entre 4km por 2km até 6km por 3km <br> b>População</b> - Entre 50 até 70 mil pessoas <br> <b>Armamentos extras</b> - Pode carregar torpedos, armas anti-projétil, aríete, canhões magnéticos e reator do tipo Último Recurso <br> <b>Pode transportar mercadorias?</b> - Não <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Não é permitido transporte de mercadorias <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Não <br> <b>Pode ter armamento anti-planetário?</b> - Somente com permissão imperial"
+
+    setaL.onclick = manualP3
+    setaR.onclick = manualP5
+}
+function manualP5() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe Cruzador</b> <br> <b>Canhões</b> - Entre 60 até 80 <br> <b>Tamanho</b> - Entre 3km por 2km até 4km por 2km <br> <b>População</b> - Entre 40 até 60 mil pessoas <br> <b>Armamentos extras</b> - Pode carregar torpedos, armas anti-projétil, aríete, canhões magnéticos e reator do tipo Último Recurso <br> <b>Pode transportar mercadorias?</b> - Não <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Não é permitido transporte de mercadorias <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Não <br> <b>Pode ter armamento anti-planetário?</b> - Não"
+
+    setaL.onclick = manualP4
+    setaR.onclick = manualP6
+}
+function manualP6() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe Cruzador Leve</b> <br> <b>Canhões</b> - Entre 50 até 56 <br> <b>Tamanho</b> - Entre 2km por 1km até 2km por 2km <br> <b>População</b> - Entre 40 até 60 mil pessoas <br> <b>Armamentos extras</b> - Pode carregar armas anti-projétil, aríete e reator do tipo Último Recurso <br> <b>Pode transportar mercadorias?</b> - Não <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Não é permitido transporte de mercadorias <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Não <br> <b>Pode ter armamento anti-planetário?</b> - Não"
+
+    setaL.onclick = manualP5
+    setaR.onclick = manualP7
+}
+function manualP7() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe Fragata</b> <br> <b>Canhões</b> - Entre 60 até 70 <br> <b>Tamanho</b> - Entre 2km por 1km até 2km por 2km <br> <b>População</b> - Entre 50 até 60 mil pessoas <br> <b>Armamentos extras</b> - Pode carregar armas carregar torpedos, anti-projétil e canhões magnéticos <br> <b>Pode transportar mercadorias?</b> - Não <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Não é permitido transporte de mercadorias <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Sim <br> <b>Pode ter armamento anti-planetário?</b> - Não"
+
+    setaL.onclick = manualP6
+    setaR.onclick = manualP8
+}
+function manualP8() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe Corveta</b> <br> <b>Canhões</b> - Entre 40 até 50 <br> <b>Tamanho</b> - Entre 1km por 1km até 2km por 2km <br> <b>População</b> - Entre 30 até 40 mil pessoas <br> <b>Armamentos extras</b> - Pode carregar armas carregar torpedos, anti-projétil e canhões magnéticos <br> <b>Pode transportar mercadorias?</b> - Não <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Não é permitido transporte de mercadorias <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Não <br> <b>Pode ter armamento anti-planetário?</b> - Não"
+
+    setaL.onclick = manualP7
+    setaR.onclick = manualP9
+}
+function manualP9() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Classe raeda</b> <br> <b>Canhões</b> - Transportadoras não podem ter armas <br> <b>Tamanho</b> - Entre 2km por 1km até 3km por 3km <br> <b>População</b> - Entre 60 até 100 mil pessoas <br> <b>Armamentos extras</b> - Transportadoras não podem ter armas <br> <b>Pode transportar mercadorias?</b> - Sim <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Sim <br> <b>Pode ser da Frota Imperial?</b> - Sim <br> <b>É obrigatoriamente da Frota Imperial?</b> - Não <br> <b>Pode ter armamento anti-planetário?</b> - Transportadoras não podem ter armas"
+
+    setaL.onclick = manualP8
+    setaR.onclick = manualP10
+}
+function manualP10() {
+    const setaL = document.getElementById("setaL");
+    const setaR = document.getElementById("setaR");
+
+    document.getElementById("consequenciasT").innerHTML = "<b>Naves genéricas</b> (não necessitam de classificação específica) <br> <b>Canhões</b> - Entre 0 até 20 <br> <b>Tamanho</b> - Entre 1km por 1km até 2km por 2km <br> <b>População</b> - Entre 20 até 30 mil pessoas <br> <b>Armamentos extras</b> - Naves genéricas não podem ter armas extras <br> <b>Pode transportar mercadorias?</b> - Sim <br> <b>É necessário ter licensa mercante para transportar mercadorias?</b> - Não <br> <b>Pode ser da Frota Imperial?</b> - Não <br> <b>É obrigatoriamente da Frota Imperial?</b> - Não pode ser da Frota Imperial <br> <b>Pode ter armamento anti-planetário?</b> - Não"
+
+    setaL.onclick = manualP9
 }
 
 // Funções que guiam a introdução
